35595627a099cb946c4921126153e284beb0882b
Karel Psota <karel.psota@clevermaps.io>
2025-02-10 10:24:04 +0100

    example

--- example.py
+++ example.py
@@ -3,6 +3,7 @@ from openapi_client.models.view_dto import ViewDTO
 with open("./data/view_example.json", "r") as file:
     json_data = file.read()
 
+# It throws validation errors in case of invalid JSON
 view_dto_instance = ViewDTO.from_json(json_data)
 
 print(view_dto_instance)
